f8fa2e009dbe529c2d25fc2b189499d0aca4b671
Yulong6 <72176987+Yulong6@users.noreply.github.com>
2020-10-04 11:26:33 -0500

    Create Shape.java

---
+++ Shape.java
@@ -0,0 +1,9 @@
+package com.example.t43_lab4;
+
+import android.graphics.drawable.shapes.Shape;
+
+public abstract class Circle extends Shape {
+    public Circle(int x, int y){
+        super(x, y);
+    }
+}
