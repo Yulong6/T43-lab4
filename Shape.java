8316cb01138c762852535bc444adaf0b6661bf7b
ziwei11 <59899776+ziwei11@users.noreply.github.com>
2020-10-04 01:55:37 -0600

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,9 @@
+package com.example.t43_lab4;
+
+import android.graphics.drawable.shapes.Shape;
+
+public abstract class Rectangle extends Shape {
+    public Rectangle(int x, int y){
+        super(x, y);
+    }
+}
